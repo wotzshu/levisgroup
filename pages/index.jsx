0038d6584d6152cf4fcb1de2d61eb79e65f84a74
wotzshu <wotzshuofficial@gmail.com>
2025-01-02 12:56:10 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -41,9 +41,9 @@ export default function Index() {
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-y-12 lg:gap-y-0 lg:gap-x-12 py-24">
           <div className="col-span-4">
             <p className="text-white text-4xl font-bold" dangerouslySetInnerHTML={{ __html:"Levis Group" }} />
-            <p className="text-white text-md font-medium text-gray-500/75" dangerouslySetInnerHTML={{ __html: "How about creating the best environment to relax yourself with your own emotions?" }}/>
+            <p className="text-white text-md font-medium text-gray-500/75" dangerouslySetInnerHTML={{ __html: "Şarkıları nasıl yükleyeceğini bilmiyor musun? YouTube hesabımızdaki videolara göz atabilirsin." }}/>
             <a href="https://www.youtube.com/@LevisGroup" className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white"}>
-              <i className="fab fa-discord mr-2" />YouTube Hesabımız
+              <i className="fab fa-youtube mr-2" />YouTube Hesabımız
             </a>
           </div>
 
@@ -51,7 +51,7 @@ export default function Index() {
            
               <div>
                 <div className="text-white">
-                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-cogs text-2xl text-amber-500`} /> ÖzeL Seçenekler</p>
+                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-cogs text-2xl text-amber-500`} /> Özel Seçenekler</p>
                   <p className="text-gray-500 line-clamp-4">Şarkılarınız sadece sizin istediğiniz platformlarda paylaşılır!</p>
                 </div>
               </div>
