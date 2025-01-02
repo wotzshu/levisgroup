cc537897ba699707521292b44751c2430f711f12
wotzshu <wotzshuofficial@gmail.com>
2025-01-02 20:01:41 +0300

    Update Header.jsx

--- components/Static/Header.jsx
+++ components/Static/Header.jsx
@@ -83,7 +83,7 @@ const Header = ({ $, NavItems }) => {
                                 width="48" height="48"
                             />
                             <p className="invisible md:visible text-xl text-white font-semibold">
-                              <a href="/"><span className="text-amber-400">Music</span>Maker</a>
+                              <a href="/"><span className="text-amber-400">Levis</span> Group</a>
                             </p>
                         </div>
                         <ul className="hidden lg:flex items-center space-x-4">
