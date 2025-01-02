d0d5205713e912c7d76396f5dc554aa8145fc2a7
wotzshu <wotzshuofficial@gmail.com>
2025-01-02 20:30:29 +0300

    Update partners.jsx

--- pages/partners.jsx
+++ pages/partners.jsx
@@ -9,21 +9,103 @@ export default function Partners() {
       </div>
                 <h1 className="py-7 animateHeader text-4xl font-extrabold text-center text-white">
                     <i className="fal fa-stars text-amber-400 mr-2" />
-                    Partners
+                    Sanatçılarımız
                 </h1>
             </div>
 
             <div className="lg:max-w-screen-lg mt-10 mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
 
             <div className="flex flex-col justify-center text-white rounded">
-            <a href="https://codeshare.me">
+            <a href="https://open.spotify.com/intl-tr/artist/3SdGkpzAA98IJUf7dwd8TD?si=UU6X4gGwSrOiqaHBHWMmVw&utm_source=copy-link&utm_medium=copy-link&nd=1&_branch_match_id=1385307170359209135&_branch_referrer=H4sIAAAAAAAAA8soKSkottLXLy7IL8lMq9TLyczL1o%2FKSCnP8QpyKXNJsq8rSk1LLSrKzEuPTyrKLy9OLbJ1zijKz00FAMAhUaI6AAAA">
                         <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
-                            <img className="rounded-full h-32 w-32" src="https://images-ext-1.discordapp.net/external/YYXCkfsbbSARvGEzBx8W_3gt7ITp4f8YB_2y14SpoiA/%3Fsize%3D4096/https/cdn.discordapp.com/icons/676124992919830548/256b9dd9a6109b1c85ca64677f84f80a.png?format=webp&quality=lossless&width=160&height=160" />
+                            <img className="rounded-full h-32 w-32" src="fotoğraf buraya" />
                             <div>
-                                <h1 className="leading-none text-3xl font-bold text-white">Code Share</h1>        
+                                <h1 className="leading-none text-3xl font-bold text-white">Metukan</h1>        
                                 <div className="flex items-center mt-1">
                                     <p className="font-normal font-sm">
-                                      Code Share, it offers an ideal platform for developers to share their projects, interact with other developers and sell their projects for money, exploring many projects in different categories and software languages.
+                                      Metukan, spotify platformunda tanınan ve sokaklar caddeler şarkısıyla ünlü olan bir sanatçımızdır.
+                                    </p>
+                                </div>     
+                            </div>
+                        </div>
+                    </a>
+                    </div>
+
+
+            </div>
+
+            <div className="lg:max-w-screen-lg mt-10 mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
+
+            <div className="flex flex-col justify-center text-white rounded">
+            <a href="https://open.spotify.com/user/31xlo3ycbjk3tv4a2veawzfoae7q">
+                        <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
+                            <img className="rounded-full h-32 w-32" src="https://instagram.fist4-1.fna.fbcdn.net/v/t51.2885-19/471512567_972598904713588_115243689164398901_n.jpg?stp=dst-jpg_s150x150_tt6&_nc_ht=instagram.fist4-1.fna.fbcdn.net&_nc_cat=104&_nc_oc=Q6cZ2AEGXtXmK0h8YXOdQY-UVso80FnNWUPMtuk8N6Jh0ZwUy2dLqoE728mAjAJOQ1BhiqY&_nc_ohc=Q-X6ZsKUzmIQ7kNvgH0mg8K&_nc_gid=2968794680614ed58495d67d22bf5cfa&edm=AP4sbd4BAAAA&ccb=7-5&oh=00_AYDj5BxwfgtZLBRbxmW2hSqZ52F_1slmg7AFFzwVTz4hqQ&oe=677C990F&_nc_sid=7a9f4b" />
+                            <div>
+                                <h1 className="leading-none text-3xl font-bold text-white">Wotzshu</h1>        
+                                <div className="flex items-center mt-1">
+                                    <p className="font-normal font-sm">
+                                      Wotzshu, spotify ve discord gibi önemli platformlarda bilinen bir sanatçımızdır.
+                                    </p>
+                                </div>     
+                            </div>
+                        </div>
+                    </a>
+                    </div>
+
+
+            </div>
+
+            <div className="lg:max-w-screen-lg mt-10 mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
+
+            <div className="flex flex-col justify-center text-white rounded">
+            <a href="https://open.spotify.com/user/31ux5ffpgpd5frnhow4yjqyt6tue?si=BHFTW9U6Sbi1q8bSuIbzYw">
+                        <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
+                            <img className="rounded-full h-32 w-32" src="https://instagram.fist4-1.fna.fbcdn.net/v/t51.2885-19/470163929_866784438682693_257148023918324118_n.jpg?stp=dst-jpg_s150x150_tt6&_nc_ht=instagram.fist4-1.fna.fbcdn.net&_nc_cat=101&_nc_oc=Q6cZ2AEleCbtugmpMIrONNXfdpjAhThJ34Z69LFAklHziSsZ_12jZL25j9t8CvQTPVICqAc&_nc_ohc=h6MtyK5pEbkQ7kNvgEOeZ8G&_nc_gid=6eff7b9d913a4a6187c9774f92e66dfd&edm=AP83FgwBAAAA&ccb=7-5&oh=00_AYBWq_Y1A0EsSsZnFFpDj5r7kVv93Fn5MNypuo9x9DivHA&oe=677C8AF4&_nc_sid=e967f1" />
+                            <div>
+                                <h1 className="leading-none text-3xl font-bold text-white">Krex</h1>        
+                                <div className="flex items-center mt-1">
+                                    <p className="font-normal font-sm">
+                                      Krex, spotify ve discord gibi önemli platformlarda bilinen bir sanatçımızdır.
+                                    </p>
+                                </div>     
+                            </div>
+                        </div>
+                    </a>
+                    </div>
+
+            </div>
+
+            <div className="lg:max-w-screen-lg mt-10 mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
+
+            <div className="flex flex-col justify-center text-white rounded">
+            <a href="https://open.spotify.com/user/31m7yddl35ehvlnlfolsztussqpq?si=R27X8ckjQJaBAsecSAPu-w">
+                        <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
+                            <img className="rounded-full h-32 w-32" src="https://arwez.com/profile-img.jpeg" />
+                            <div>
+                                <h1 className="leading-none text-3xl font-bold text-white">Ardgull</h1>        
+                                <div className="flex items-center mt-1">
+                                    <p className="font-normal font-sm">
+                                      Ardgull, spotify ve discord gibi önemli platformlarda bilinen bir sanatçımızdır.
+                                    </p>
+                                </div>     
+                            </div>
+                        </div>
+                    </a>
+                    </div>
+
+            </div>
+
+            <div className="lg:max-w-screen-lg mt-10 mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
+
+            <div className="flex flex-col justify-center text-white rounded">
+            <a href="https://open.spotify.com/user/31m22gc5etpmuvcy6zmoonjueruu">
+                        <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
+                            <img className="rounded-full h-32 w-32" src="https://instagram.fist4-1.fna.fbcdn.net/v/t51.2885-19/467489051_3797789083882781_2561624323718466448_n.jpg?stp=dst-jpg_s150x150_tt6&_nc_ht=instagram.fist4-1.fna.fbcdn.net&_nc_cat=105&_nc_oc=Q6cZ2AGMKVKR_iVBNLYKiIalEs-o2TB74qpE4tm3KN4vpJCZIS3ZcX16-gkHT71QT5im4qk&_nc_ohc=RrgOLn2k9m8Q7kNvgGmUZDo&_nc_gid=dae7d6b623db4dd4be9c477281333083&edm=ALGbJPMBAAAA&ccb=7-5&oh=00_AYBXE3MFeRCpviO-a8zv9kkyZvRgz1PAhtMI4jufISwgxQ&oe=677CB3CE&_nc_sid=7d3ac5" />
+                            <div>
+                                <h1 className="leading-none text-3xl font-bold text-white">Umutbllbs</h1>        
+                                <div className="flex items-center mt-1">
+                                    <p className="font-normal font-sm">
+                                      Umutbllbs, spotify ve discord gibi önemli platformlarda bilinen bir sanatçımızdır.
                                     </p>
                                 </div>     
                             </div>
