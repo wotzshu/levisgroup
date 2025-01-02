32d31b173e987fb0817d3fac2a746fd57cddbebe
wotzshu <wotzshuofficial@gmail.com>
2025-01-02 12:17:05 +0300

    Update 404.jsx

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,9 +1,9 @@
 import ErrorPage from '../components/ErrorPage';
 const UnkownPage = () => {
     let messages = [
-        'Have you lost your way, kiddo?',
-        'Damn it! This dead-end road.',
-        'The Aurors blocked this road, Harry!'
+        'Bulunmayan bir sayfaya girmeye çalışıyorsun!',
+        'Bulunmayan bir sayfaya girmeye çalışıyorsun!',
+        'Bulunmayan bir sayfaya girmeye çalışıyorsun!'
     ];
 
     return <ErrorPage code={404} message={messages[Math.floor(Math.random()*messages.length)] || "Page not found."} />
